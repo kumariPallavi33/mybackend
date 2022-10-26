7d7f052f5b7ce3af720d7adb7a06b87bed6d889c
kumariPallavi33 <kpallaviyadav33@gmail.com>
2022-10-26 18:37:10 +0530

    ntng only connecting mongo

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://kumaripallavi:RzcMX4inoaypDFUx@cluster0.qxkskek.mongodb.net/kumariPallavi33", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
